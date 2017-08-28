45c97ef0f1f9f3b7e0b22a7af48d56d66fa09857
Jacek Siciarek <siciarek@gmail.com>
2017-08-28 17:34:03 +0200

    Add viewer and editor

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -4,54 +4,9 @@ import PropTypes from 'prop-types';
 
 const Home = (props) => {
 
-  const configs = [
-    {
-      project: 'ino',
-      branch: 'master',
-    },
-    {
-      project: 'ino',
-      branch: 'test',
-    },
-    {
-      project: 'productcatalog',
-      branch: 'master',
-    },
-    {
-      project: 'productcatalog',
-      branch: 'test',
-    },
-    {
-      project: 'taskbox',
-      branch: 'master',
-    },
-    {
-      project: 'taskbox',
-      branch: 'test',
-    },
-  ]
-
-  const {router} = props
-
   return (
     <section>
-      <nav className="panel">
-        <p className="panel-heading">
-          <strong>{`Config list`.toUpperCase()}</strong>
-        </p>
-        {
-          configs.map(({project, branch}, i) => (
-            <a key={i} className="panel-block" onClick={() => router.push(`/editor/${project}/${branch}`)}>
-              <span className="panel-icon">
-                <i className="fa fa-cog"></i>
-              </span>
-              <strong>{project.toUpperCase()}</strong>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              {branch}
-            </a>
-          ))
-        }
-      </nav>
+      <h1>Home</h1>
     </section>
   )
 }
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import {syncHistoryWithStore} from 'react-router-redux'
 import store from './app/store'
 import routes from './app/routes'
 
-// ReactDOM.render(<App children={<View name={'INO'} description={'master'} definition={definition}/>} />, document.getElementById('root'));
-
 ReactDOM.render(
   <Provider store={store}>
     <Router history={syncHistoryWithStore(routerHistory, store)} routes={routes}/>
