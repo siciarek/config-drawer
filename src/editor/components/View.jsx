855ecfb3f7e41554779bf3776b59313a11494577
Jacek Siciarek <siciarek@gmail.com>
2017-08-29 10:34:27 +0200

    Format view in INI format

--- src/editor/components/View.jsx
+++ src/editor/components/View.jsx
@@ -1,21 +1,62 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const parseValue = (val) => {
+  switch (typeof val) {
+    case 'string':
+    case 'number':
+      return val;
+    case 'boolean':
+      return JSON.stringify(val)
+  }
+
+  return `****************************** ${JSON.stringify(val)} [${typeof val}] ******************************`
+}
+
+const parseKeyValue = (key, val) => {
+  if (Array.isArray(val)) {
+    const output = val.map((v, i) => {
+      return `${key}[]=${v}`
+    })
+    return output.join("\n")
+  }
+  return `${key}=${parseValue(val)}`
+}
+
 class View extends React.Component {
 
   render() {
 
-    const {name, description, definition} = this.props
+    const {name, description, definition, router} = this.props
+
+    if (!definition) {
+      router.push('/')
+      return null
+    }
 
     return (
-      <section>
-        <nav className="panel">
-          <p className="panel-heading">
-            <strong>{name.toUpperCase()}</strong> {description}
-          </p>
-          <pre className="panel-block">{JSON.stringify(definition, null, 4)}</pre>
-        </nav>
-      </section>
+      <pre className="panel-block">
+          {
+            `; Application configuration file: project=${name}, branch=${description}
+; Version: 1
+
+`
+          }
+        {
+          Object.keys(definition).map((key, i) => {
+            const value = definition[key]
+            const section = `[${key}]`
+            const values = Object.keys(value).map((key, i) => {
+              return parseKeyValue(key, value[key])
+            })
+
+            const temp = values.join("\n")
+            const sectionValues = temp.length === 0 ? '; SECTION IS EMPTY' : temp
+
+            return `${section}\n${sectionValues}\n\n`
+          })
+        }
+        </pre>
     )
   }
 }
